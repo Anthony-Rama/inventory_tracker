b302701161bfc0200d1ef844e9097655a34317e3
Anthony Rama <anthonyhrama@gmail.com>
2024-08-06 22:08:12 -0400

    Added logic and styles to the page for adding to inventory

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import { useState, useEffect} from 'react'
 import{firestore} from '@/firebase'
-import {Box, Typography} from '@mui/material'
+import {Box, Modal, Typography, Stack, TextField, Button} from '@mui/material'
 import { collection, getDocs, query, updateDoc } from "firebase/firestore";
 
 export default function Home() {
@@ -70,16 +70,51 @@ export default function Home() {
 
 
   return (
-    <Box>
+    <Box 
+    width = "100vw" 
+    height = "100vh" 
+    display = "flex"
+    justifyContent = "center" 
+    alignItems = "center" 
+    gap = {2}
+    >
+      <Modal open = {open} onClosed = {handleClose}> 
+        <Box 
+        position = "absolute" 
+        top = "50%" 
+        left = "50%" 
+        width = {400} 
+        bgcolor = "white" 
+        border = "2px solid #000" 
+        boxShadow = {24} 
+        p = {4} 
+        display = "flex" 
+        flexDirection = "column" 
+        gap = {3} 
+        sx = {{ 
+          transform: "translate(-50%, -50%)", 
+        }}
+        >
+          <Typography variant = "h6">Add Item</Typography> 
+          <Stack width = "100%" direction = "row" spacing = {2}>
+            <TextField 
+            variant = 'outlined'
+            fullWidth
+            value = {itemName}
+            onChange = {(e) => setItemName(e.target.value)}
+            />
+            <Button
+            variant = "outlined" 
+            onClick={() => {
+              addItems(itemName);
+              setItemName('');
+              handleClose();
+            }}
+            >Add</Button>
+          </Stack>
+        </Box>
+      </Modal>
       <Typography variant="h1">Inventory Tracker</Typography>
-      {inventory.forEach((item) => {
-        console.log(item)
-          return(<>
-          {item.name}
-          {item.count}
-          </>)
-        })
-      }
     </Box>
-  )
+  );
 }
