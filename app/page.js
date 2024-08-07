6ec7c1f5bbe9930623e153a685d1dd97f2039cad
Anthony Rama <anthonyhrama@gmail.com>
2024-08-06 20:20:57 -0400

    Edited page.js to implement backend logic for get, add, and remove inventory

--- app/page.js
+++ app/page.js
@@ -3,8 +3,7 @@ import Image from "next/image";
 import { useState, useEffect} from 'react'
 import{firestore} from '@/firebase'
 import {Box, Typography} from '@mui/material'
-import { query } from "firebase/firestore";
-
+import { collection, getDocs, query, updateDoc } from "firebase/firestore";
 
 export default function Home() {
   // Declare Helper Functions
@@ -12,14 +11,75 @@ export default function Home() {
   const [open, setOpen] = useState(false) // state variable to add & remove items
   const [itemName, setItemName] = useState('') // state variable to store name of items
 
-  // Functions
-  const updateInventory = async () => {
-    const snapshot = query(collection(firestore, 'inventory'))
+  // -- Functions -- 
+
+  // Function #1: Update Inventory 
+  const updateInventory = async () => { 
+    const snapshot = query(collection(firestore, 'inventory')) 
+    const docs = await getDocs(snapshot)
+    const inventoryList = []
+    docs.forEach((doc)=>{
+      inventoryList.push({
+        name: doc.id,
+        ...doc.data(),
+      })
+    })
+    setInventory(inventoryList)
   }
+
+  // Function #2: Add Items
+  const addItems = async (item) => {
+    const docRef = doc(collection(firestore, 'inventory'), item)
+    const docSnap = await getDoc(docRef)
+
+    if(docSnap.exists()){
+      const {quantity} = docSnap.data()
+      await setDoc(docRef, {quantity: quantity + 1})
+      } else {
+        await setDoc(docRef, {quantity: 1})
+      }
+
+    await updateInventory()
+  }
+
+  // Function #3: Remove Items
+  const removeItem = async (item) => {
+    const docRef = doc(collection(firestore, 'inventory'), item)
+    const docSnap = await getDoc(docRef)
+
+    if(docSnap.exists()){
+      const {quantity} = docSnap.data()
+      if (quantity === 1) {
+        await deleteDoc(docRef)
+      } else{ 
+        await setDoc(docRef, {quantity: quantity - 1})
+      }
+    }
+
+    await updateInventory()
+  }
+
+  useEffect(() => {
+    updateInventory()
+  }, [])
+
+
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
+
 
   return (
     <Box>
       <Typography variant="h1">Inventory Tracker</Typography>
+      {inventory.forEach((item) => {
+        console.log(item)
+          return(<>
+          {item.name}
+          {item.count}
+          </>)
+        })
+      }
     </Box>
   )
 }
